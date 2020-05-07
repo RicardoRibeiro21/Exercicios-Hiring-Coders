cecf01459178f4e407f8eab0626ccebdf9fb160a
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-05-07 15:22:32 -0300

    Exercicios de operação

--- JavaScript/numeros.js
+++ JavaScript/numeros.js
@@ -1,2 +1,23 @@
 // Módulo JavaScript 1
 //Exercício com números 
+
+//Números
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado);
+
+//Texto
+var nome = "Ricardo";
+var sobrenome = "Ribeiro";
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+//Subtração 
+var primeiroValor = -10;
+var segundoValor = '20';
+var resultSubtracao = primeiroValor - segundoValor;
+console.log(resultSubtracao);
