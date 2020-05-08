a51280358e4f2c322ecdfff5bec3d95afcb2d664
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-05-07 22:28:57 -0300

    Functions

---
+++ JavaScript/classe.js
@@ -0,0 +1,16 @@
+class Matematica {
+    soma(n1, n2){        
+        return n1 + n2;;
+    }
+    multiplicacao (n1, n2){
+        return n1 * n2;
+    }
+    divisao (n1, n2){
+        return n1 / n2;
+    }    
+}
+
+//Instanciando classe em JavaScript
+var instanciaMat = new Matematica();
+
+var resultado = soma(10, 10);
---
+++ JavaScript/funcoes.js
@@ -0,0 +1,7 @@
+function soma (num1, num2) {
+    var resultado = num1 + num2;
+    return resultado;
+}
+
+var resultadoSoma = soma(12, 21);
+console.log(resultadoSoma);
